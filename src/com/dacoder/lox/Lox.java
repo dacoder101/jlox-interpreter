2f2e61fc7a521d92533c74f692bd415b69bc59fb
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-10-02 05:36:49 +0000

    Remove unused exception

--- src/com/dacoder/lox/Lox.java
+++ src/com/dacoder/lox/Lox.java
@@ -1,7 +1,6 @@
 package com.dacoder.lox;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
